601539f8d129db7d13190c3693730c53090b3c8b
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2026-01-10 12:23:57 +0100

    remove migration version

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 20_240_101_000_000) do
+ActiveRecord::Schema.define(version: 20_240_101_000_000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
