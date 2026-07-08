c5775bbb2dd15cfb675f97ea92627d7153a0b099
Luiz Eduardo Kowalski <luizeduardokowalski@gmail.com>
2026-07-08 14:26:57 +0200

    fix: workaround for oxidize-rb/rb-sys/issues/745

--- ext/snowflaked/extconf.rb
+++ ext/snowflaked/extconf.rb
@@ -4,3 +4,6 @@ require "mkmf"
 require "rb_sys/mkmf"
 
 create_rust_makefile("snowflaked/snowflaked")
+
+makefile = File.read("Makefile")
+File.write("Makefile", makefile) if makefile.gsub!('install_name_tool -id ""', 'install_name_tool -id "$(notdir $(DLLIB))"')
